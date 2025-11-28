3b9149ab1cdf571c4ad08fb55a864eeceb165412
caul137 <salomao.caua@gmail.com>
2025-11-28 15:10:34 -0300

    new

--- src/app.service.ts
+++ src/app.service.ts
@@ -1,8 +1,15 @@
 import { Injectable } from '@nestjs/common';
+import { metrics } from './tracer';
 
 @Injectable()
 export class AppService {
   getHello(): string {
+    const metric = metrics.getMeter('api-service')
+    const successMetric = metric.createCounter('hello_success')
+    successMetric.add[1]
+
+    const histogram = metric.createHistogram('request_validated')
+    histogram.record(1000)
     return 'Hello World!';
   }
 }
--- src/tracer.ts
+++ src/tracer.ts
@@ -1,46 +1,56 @@
-
 import { NodeSDK } from '@opentelemetry/sdk-node';
-import { ConsoleSpanExporter, BatchSpanProcessor } from '@opentelemetry/sdk-trace-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import {
+  MeterProvider,
   PeriodicExportingMetricReader,
-  ConsoleMetricExporter,
 } from '@opentelemetry/sdk-metrics';
 import { resourceFromAttributes } from '@opentelemetry/resources';
-import {OTLPTraceExporter} from '@opentelemetry/exporter-trace-otlp-proto'
+import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-proto';
 import {
-    ATTR_SERVICE_NAME,
-    ATTR_SERVICE_VERSION,
-} from '@opentelemetry/semantic-conventions'
-import {SimpleLogRecordProcessor} from '@opentelemetry/sdk-logs'
-import { OTLPLogExporter } from "@opentelemetry/exporter-logs-otlp-proto";
+  ATTR_SERVICE_NAME,
+  ATTR_SERVICE_VERSION,
+} from '@opentelemetry/semantic-conventions';
 
+import { OTLPMetricExporter } from '@opentelemetry/exporter-metrics-otlp-grpc';
 
-import {diag, DiagConsoleLogger, DiagLogLevel} from '@opentelemetry/api'
+import {
+  diag,
+  DiagConsoleLogger,
+  DiagLogLevel,
+  metrics,
+} from '@opentelemetry/api';
 
-console.log('a')
+const SERVICE_NAME = 'api-service';
+
+const metricsExporter = new OTLPMetricExporter({
+  url: 'http://127.0.0.1:4317',
+});
+const metricReader = new PeriodicExportingMetricReader({
+  exporter: metricsExporter,
+  exportIntervalMillis: 1000,
+});
 
 const traceExporter = new OTLPTraceExporter();
 
 const resource = resourceFromAttributes({
-    [ATTR_SERVICE_NAME]: 'api-service',
-    [ATTR_SERVICE_VERSION]: '1.0.0',
-})
-
-const mergedResource = resource;
-
-diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO)
-
-const sdk = new NodeSDK({
-  spanProcessors:[ new BatchSpanProcessor(traceExporter)],
-  traceExporter: new ConsoleSpanExporter(),
-  metricReaders:[ new PeriodicExportingMetricReader({
-    exporter: new ConsoleMetricExporter(),
-  })],
-  logRecordProcessors: [new SimpleLogRecordProcessor(new OTLPLogExporter())],
-  instrumentations: [getNodeAutoInstrumentations()],
-  resource: mergedResource
+  [ATTR_SERVICE_NAME]: 'api-service',
+  [ATTR_SERVICE_VERSION]: '1.0.0',
 });
 
+const mergedResource = resource;
+diag.setLogger(new DiagConsoleLogger(), DiagLogLevel.INFO);
 
-export default sdk
+const sdk = new NodeSDK({
+  traceExporter,
+  instrumentations: [getNodeAutoInstrumentations()],
+  resource: mergedResource,
+  serviceName: SERVICE_NAME,
+});
+
+const meterProvider = new MeterProvider({
+  resource: mergedResource,
+  readers: [metricReader],
+});
+metrics.setGlobalMeterProvider(meterProvider);
+
+export {sdk, metrics};
